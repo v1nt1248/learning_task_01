da1b089a170e0209cfe5aec06dbba5e9d4b5d742
vitaly <Vitali.Kashkel@sibel.by>
2017-03-22 10:52:03 +0300

    edit .gitignore file

---
+++ src/@types/test/index.d.ts
@@ -0,0 +1,23 @@
+/* v1nt1248 22.03 */
+
+declare namespace test {
+
+  interface Coords {
+    x: number;
+    y: number;
+  }
+
+  interface Size {
+    width: number;
+    height: number;
+  }
+
+  interface Geometric extends Coords, Size {
+  }  
+
+  interface Amt {
+    column: number;
+    row: number;
+  }
+
+}
